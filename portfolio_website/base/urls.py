ce65d21f3de2561db2885427a56ab1c668641244
Gabriel Vieira1 <devgvieira@github.com>
2024-01-23 18:41:27 -0300

    version 1

--- portfolio_website/base/urls.py
+++ portfolio_website/base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('download_my_pdf', views.download_pdf, name="download_pdf")
 ]
--- portfolio_website/base/views.py
+++ portfolio_website/base/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from wsgiref.util import FileWrapper
+import os
 
 def home(request):
     return render(request, 'base/home.html')
+
+def download_pdf(request):
+    filename = 'staticfiles/files/CV.pdf'
+    with open(filename, 'rb') as pdf_file:
+        content = FileWrapper(pdf_file)
+        response = HttpResponse(content, content_type='application/pdf')
+        response['Content-Length'] = os.path.getsize(filename)
+        response['Content-Disposition'] = 'attachment; filename=%s' % 'CV.pdf'
+    return response
